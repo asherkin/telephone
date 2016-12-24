afaf5f964e1b11f80b0e391ea2f9b234eb742852
Asher Baker <asherkin@limetech.org>
2016-12-24 16:20:34 +0000

    Implement new helpers and Steam frame decoding

--- web-client/worker.js
+++ web-client/worker.js
@@ -1,6 +1,10 @@
 var Module = {};
 importScripts('decoders.js');
 
+/*
+ * Native functions.
+ */
+
 // void *malloc(size_t size);
 var malloc = Module.cwrap('malloc', 'number', ['number']);
 
@@ -9,6 +13,18 @@ var free = Module.cwrap('free', null, ['number']);
 
 // uint32_t crc32_16bytes(const void *data, size_t length, uint32_t previousCrc32 = 0);
 var crc32_16bytes = Module.cwrap('crc32_16bytes', 'number', ['number', 'number', 'number']);
+
+// SRC_STATE *src_new(int converter_type, int channels, int *error);
+var src_new = Module.cwrap('src_new', 'number', ['number', 'number', 'number']);;
+
+// SRC_STATE *src_delete(SRC_STATE *state);
+var src_delete = Module.cwrap('src_delete', 'number', ['number']);;
+
+// int src_process(SRC_STATE *state, SRC_DATA *data);
+var src_process = Module.cwrap('src_process', 'number', ['number', 'number']);;
+
+// void src_short_to_float_array(const short *in, float *out, int len);
+var src_short_to_float_array = Module.cwrap('src_short_to_float_array', null, ['number', 'number', 'number']);;
 
 // SKP_int SKP_Silk_SDK_Get_Decoder_Size(SKP_int32 *decSizeBytes);
 var SKP_Silk_SDK_Get_Decoder_Size = Module.cwrap('SKP_Silk_SDK_Get_Decoder_Size', 'number', ['number']);
@@ -61,194 +77,13 @@ var celt_decode_float = Module.cwrap('celt_decode_float', 'number', ['number', '
 // int celt_decoder_ctl(CELTDecoder *st, int request, ...);
 var celt_decoder_ctl = Module.cwrap('celt_decoder_ctl', 'number', ['number', 'number']);
 
-var ptrDecoderSize = malloc(4);
-SKP_Silk_SDK_Get_Decoder_Size(ptrDecoderSize);
-var decoderSize = Module.getValue(ptrDecoderSize, 'i32')
-//console.log('Decoder state size: ' + decoderSize);
-free(ptrDecoderSize);
+/*
+ * Native constants.
+ */
 
-var ptrDecoderControl = malloc(4 * 5);
-Module.setValue(ptrDecoderControl, 16000, 'i32'); // API_sampleRate
-Module.setValue(ptrDecoderControl + (4 * 1), 0, 'i32'); // frameSize
-Module.setValue(ptrDecoderControl + (4 * 2), 0, 'i32'); // framesPerPacket
-Module.setValue(ptrDecoderControl + (4 * 3), 0, 'i32'); // moreInternalDecoderFrames
-Module.setValue(ptrDecoderControl + (4 * 4), 0, 'i32'); // inBandFECOffset
-
-var maxSamples = 8192;
-var ptrSamplesOut = malloc(maxSamples << 1);
-
-var ptrSampleCount = malloc(2);
-
-var decoders = [];
-
-function renderSteamId(steamAccountFlags, steamAccountId) {
-  if (steamAccountFlags === 0x01100001) {
-    return '[U:1:' + steamAccountId + ']';
-  }
-  return steamAccountFlags + '-' + steamAccountId;
-}
-
-function getOrCreateDecoderForSteamId(steamAccountFlags, steamAccountId) {
-  var bucket = decoders[steamAccountFlags];
-  if (typeof bucket === 'undefined') {
-    bucket = decoders[steamAccountFlags] = [];
-  }
-
-  var decoder = bucket[steamAccountId];
-  if (typeof decoder === 'undefined') {
-    decoder = bucket[steamAccountId] = malloc(decoderSize);
-    SKP_Silk_SDK_InitDecoder(decoder);
-    console.log('Initializing new decoder for ' + renderSteamId(steamAccountFlags, steamAccountId));
-  }
-
-  return {
-    steamAccountFlags: steamAccountFlags,
-    steamAccountId: steamAccountId,
-    ptrDecoderState: decoder,
-  };
-}
-
-function decodeSilkData(decoder, ptrDataIn, dataLength) {
-  var cursor = 0;
-
-  var sampleCount = 0;
-  Module.setValue(ptrSampleCount, maxSamples, 'i16');
-
-  while (cursor < dataLength) {
-    var length = (Module.getValue(ptrDataIn + cursor, 'i8') & 0x00FF) | ((Module.getValue(ptrDataIn + cursor + 1, 'i8') << 8) & 0xFF00);
-    cursor += 2;
-    //console.log('Chunk length: ' + length);
-
-    if (length === 0) {
-      continue;
-    }
-
-    if (length === -1) {
-      SKP_Silk_SDK_InitDecoder(decoder.ptrDecoderState);
-      continue;
-    }
-
-    var moreInternalDecoderFrames = 0;
-    do {
-      Module.setValue(ptrSampleCount, maxSamples - sampleCount, 'i16');
-
-      var ret = SKP_Silk_SDK_Decode(decoder.ptrDecoderState, ptrDecoderControl, 0, ptrDataIn + cursor, length, ptrSamplesOut + (sampleCount << 1), ptrSampleCount);
-      if (ret !== 0) {
-        postMessage({
-          type: 'decode-error',
-          error: ret,
-        });
-      }
-
-      var decodedSamples = Module.getValue(ptrSampleCount, 'i16');
-      sampleCount += decodedSamples;
-
-      moreInternalDecoderFrames = Module.getValue(ptrDecoderControl + (4 * 3), 'i32');
-
-      //console.log('Decoded ' + decodedSamples + ' samples.');
-    } while (moreInternalDecoderFrames > 0 && sampleCount <= maxSamples);
-
-    cursor += length;
-  }
-
-  if (sampleCount > 0) {
-    var sampleRate = Module.getValue(ptrDecoderControl, 'i32');
-    //var samplesOut = Module.HEAP16.subarray(ptrSamplesOut >> 1, (ptrSamplesOut >> 1) + sampleCount);
-    //var samplesOut = Module.HEAPU8.buffer.slice(ptrSamplesOut, ptrSamplesOut + (sampleCount << 1));
-
-    var samplesOut = new Float32Array(sampleCount * 2);
-    for (var i = 0; i < sampleCount; ++i) {
-      var sample = Module.HEAP16[(ptrSamplesOut >> 1) + i] / 0xFFFF;
-      samplesOut[i * 2] = sample;
-      samplesOut[(i * 2) + 1] = sample;
-    }
-
-    postMessage({
-      type: 'audio-data',
-      id: decoder.ptrDecoderState,
-      avatar: (decoder.steamAccountFlags === 0x01100001) ? decoder.steamAccountId : null,
-      sampleRate: sampleRate * 2,
-      samples: samplesOut,
-    }, [
-      samplesOut.buffer,
-    ]);
-  }
-}
-
-function processVoicePacket(ptrDataIn, dataLength) {
-  var cursor = 0;
-
-  var steamAccountId =
-    ((Module.getValue(ptrDataIn + cursor + 0, 'i8') << (8 * 0)) & 0x000000FF) |
-    ((Module.getValue(ptrDataIn + cursor + 1, 'i8') << (8 * 1)) & 0x0000FF00) |
-    ((Module.getValue(ptrDataIn + cursor + 2, 'i8') << (8 * 2)) & 0x00FF0000) |
-    ((Module.getValue(ptrDataIn + cursor + 3, 'i8') << (8 * 3)) & 0xFF000000);
-  cursor += 4;
-
-  var steamAccountFlags =
-    ((Module.getValue(ptrDataIn + cursor + 0, 'i8') << (8 * 0)) & 0x000000FF) |
-    ((Module.getValue(ptrDataIn + cursor + 1, 'i8') << (8 * 1)) & 0x0000FF00) |
-    ((Module.getValue(ptrDataIn + cursor + 2, 'i8') << (8 * 2)) & 0x00FF0000) |
-    ((Module.getValue(ptrDataIn + cursor + 3, 'i8') << (8 * 3)) & 0xFF000000);
-  cursor += 4;
-
-  var decoder = getOrCreateDecoderForSteamId(steamAccountFlags, steamAccountId);
-
-  dataLength -= 4; // The CRC32 at the end.
-
-  while (cursor < dataLength) {
-    var payloadType = Module.getValue(ptrDataIn + cursor, 'i8');
-    cursor += 1;
-
-    switch (payloadType) {
-      case 0: {
-        var numSamples = (Module.getValue(ptrDataIn + cursor, 'i8') & 0x00FF) | ((Module.getValue(ptrDataIn + cursor + 1, 'i8') << 8) & 0xFF00);
-        cursor += 2;
-        break;
-      }
-
-      case 1:
-      case 2:
-      case 3:
-      case 4: {
-        var length = (Module.getValue(ptrDataIn + cursor, 'i8') & 0x00FF) | ((Module.getValue(ptrDataIn + cursor + 1, 'i8') << 8) & 0xFF00);
-        cursor += 2;
-
-        if (payloadType === 4) {
-          decodeSilkData(decoder, ptrDataIn + cursor, length);
-        } else {
-          postMessage({
-            type: 'unsupported-codec',
-            codec: payloadType,
-          });
-        }
-
-        cursor += length;
-        break;
-      }
-
-      case 10: {
-        cursor += 2;
-        break;
-      }
-
-      case 11: {
-        var sampleRate = (Module.getValue(ptrDataIn + cursor, 'i8') & 0x00FF) | ((Module.getValue(ptrDataIn + cursor + 1, 'i8') << 8) & 0xFF00);
-        cursor += 2;
-        Module.setValue(ptrDecoderControl, sampleRate, 'i32');
-        break;
-      }
-
-      default: {
-        postMessage({
-          type: 'unknown-payload',
-          payload: payloadType,
-        });
-        break;
-      }
-    }
-  }
-}
+/*
+ * Constants and helper functions.
+ */
 
 var TelephoneEvent = {
   VoiceData: 0,
@@ -260,6 +95,211 @@ var VoiceDataType = {
   Celt:  2,
 };
 
+var VoiceDataTypeNames = [
+  "Steam (SILK)",
+  "Speex",
+  "CELT",
+];
+
+function readInt8(ptr) {
+  return (Module.HEAPU8[ptr] & 0xFF);
+}
+
+function readInt16(ptr) {
+  return ((Module.HEAPU8[ptr    ]     ) & 0x00FF) |
+         ((Module.HEAPU8[ptr + 1] << 8) & 0xFF00);
+}
+
+function readInt32(ptr) {
+  return ((Module.HEAPU8[ptr    ]      ) & 0x000000FF) |
+         ((Module.HEAPU8[ptr + 1] <<  8) & 0x0000FF00) |
+         ((Module.HEAPU8[ptr + 2] << 16) & 0x00FF0000) |
+         ((Module.HEAPU8[ptr + 3] << 24) & 0xFF000000);
+}
+
+function renderSteamId(steamAccountFlags, steamAccountId) {
+  if (steamAccountFlags === 0x01100001) {
+    return '[U:1:' + steamAccountId + ']';
+  }
+  return steamAccountFlags + '-' + steamAccountId;
+}
+
+/*
+ * Decoder classes.
+ */
+
+var SteamDecoder = function() {
+  throw new Error('Steam (SILK) decoder not implemented.');
+
+  this.sampleRate = 16000;
+};
+
+SteamDecoder.prototype.release = function() {
+
+};
+
+SteamDecoder.prototype.process = function(ptrDataIn, dataLength) {
+  // Drop the CRC32 from the end, we're not going to check it.
+  dataLength -= 4;
+
+  var cursor = 0;
+  while (cursor < dataLength) {
+    var payloadType = readInt8(ptrDataIn + cursor);
+    cursor += 1;
+
+    switch (payloadType) {
+      // Silence.
+      case 0: {
+        var numSamples = readInt16(ptrDataIn + cursor);
+        cursor += 2;
+
+        // TODO: Send an empty audio buffer to play.
+
+        break;
+      }
+
+      // Voice data in various codecs.
+      case 1:   // Unknown (Miles?)
+      case 2:   // Speex
+      case 3:   // Uncompressed PCM
+      case 4: { // SILK
+        var length = readInt16(ptrDataIn + cursor);
+        cursor += 2;
+
+        if (payloadType === 4) {
+          //decodeSilkData(decoder, ptrDataIn + cursor, length);
+        } else {
+          postMessage({
+            type: 'unsupported-codec',
+            codec: payloadType,
+          });
+        }
+
+        cursor += length;
+        break;
+      }
+
+      // Unknown payload, maybe an older version of 11?
+      /*case 10: {
+        cursor += 2;
+        break;
+      }*/
+
+      // Set sample rate.
+      case 11: {
+        var sampleRate = readInt16(ptrDataIn + cursor);
+        cursor += 2;
+
+        // TODO: Just store this in the decoder state?
+        this.sampleRate = sampleRate;
+
+        break;
+      }
+
+      default: {
+        postMessage({
+          type: 'unknown-payload',
+          payload: payloadType,
+        });
+        return; // We need to bail out, else we'll be misaligned.
+      }
+    }
+  }
+};
+
+var SpeexDecoder = function() {
+  throw new Error('Speex decoder not implemented.');
+};
+
+SpeexDecoder.prototype.release = function() {
+
+};
+
+SpeexDecoder.prototype.process = function(ptrDataIn, dataLength) {
+
+};
+
+var CeltDecoder = function() {
+  throw new Error('CELT decoder not implemented.');
+};
+
+CeltDecoder.prototype.release = function() {
+
+};
+
+CeltDecoder.prototype.process = function(ptrDataIn, dataLength) {
+
+};
+
+/*
+ * Global state.
+ */
+
+var websocket = null;
+var decoders = [];
+var outputSampleRate = 48000;
+
+/*
+ * The meat.
+ */
+
+function getOrCreateDecoder(voiceDataType, steamAccountFlags, steamAccountId) {
+  var outerBucket = decoders[voiceDataType];
+  if (typeof outerBucket === 'undefined') {
+    outerBucket = decoders[voiceDataType] = [];
+  }
+
+  var innerBucket = outerBucket[steamAccountFlags];
+  if (typeof innerBucket === 'undefined') {
+    innerBucket = outerBucket[steamAccountFlags] = [];
+  }
+
+  var decoder = innerBucket[steamAccountId];
+  if (typeof decoder === 'undefined') {
+    console.log('Initializing new ' + VoiceDataTypeNames[voiceDataType] + ' decoder for ' + renderSteamId(steamAccountFlags, steamAccountId));
+
+    switch (voiceDataType) {
+      case VoiceDataType.Steam: {
+        decoder = innerBucket[steamAccountId] = new SteamDecoder();
+        break;
+      }
+      case VoiceDataType.Speex: {
+        decoder = innerBucket[steamAccountId] = new SpeexDecoder();
+        break;
+      }
+      case VoiceDataType.Celt: {
+        decoder = innerBucket[steamAccountId] = new CeltDecoder();
+        break;
+      }
+    }
+  }
+
+  return decoder;
+}
+
+function processVoicePacket(ptrDataIn, dataLength) {
+  var cursor = 0;
+
+  var voiceDataType = readInt8(ptrDataIn + cursor);
+  cursor += 1;
+
+  var steamAccountId = readInt32(ptrDataIn + cursor);
+  cursor += 4;
+
+  var steamAccountFlags = readInt32(ptrDataIn + cursor);
+  cursor += 4;
+
+  var decoder = getOrCreateDecoder(voiceDataType, steamAccountFlags, steamAccountId);
+  if (!decoder) {
+    postMessage({
+      type: 'unknown-decoder',
+      decoder: voiceDataType,
+    });
+  }
+
+  decoder.process(ptrDataIn + cursor, dataLength - cursor);
+}
+
 function processPacket(e) {
   var dataLength = e.data.byteLength;
   var ptrDataIn = malloc(dataLength);
@@ -267,10 +307,7 @@ function processPacket(e) {
 
   var cursor = 0;
 
-  var telephoneEvent = Module.getValue(ptrDataIn + cursor, 'i8');
-  cursor += 1;
-
-  var voiceDataType = Module.getValue(ptrDataIn + cursor, 'i8');
+  var telephoneEvent = readInt8(ptrDataIn + cursor);
   cursor += 1;
 
   switch (telephoneEvent) {
@@ -280,8 +317,8 @@ function processPacket(e) {
     }
     default: {
       postMessage({
-        type: 'unknown-packet',
-        packet: packetType,
+        type: 'unknown-event',
+        event: telephoneEvent,
       });
       break;
     }
@@ -289,8 +326,6 @@ function processPacket(e) {
 
   free(ptrDataIn);
 };
-
-var websocket = null;
 
 function onWebsocketOpen(e) {
   postMessage({
@@ -319,17 +354,21 @@ function disconnect() {
     websocket.close();
   }
 
-  for (var bucket in decoders) {
-    for (var decoder in bucket) {
-      free(decoders[bucket][decoder]);
+  for (var outerBucket in decoders) {
+    for (var innerBucket in decoders[outerBucket]) {
+      for (var decoder in decoders[outerBucket][innerBucket]) {
+        decoders[outerBucket][innerBucket][decoder].release();
+      }
     }
   }
 
   decoders = [];
 }
 
-function connectToServer(address) {
+function connectToServer(address, sampleRate) {
   disconnect();
+
+  outputSampleRate = sampleRate;
 
   websocket = new WebSocket(address, 'telephone');
   websocket.binaryType = 'arraybuffer';
@@ -339,10 +378,10 @@ function connectToServer(address) {
   websocket.onmessage = processPacket;
 }
 
-onmessage = function(e) {
+self.onmessage = function(e) {
   switch(e.data.type) {
     case 'connect':
-      connectToServer(e.data.address);
+      connectToServer(e.data.address, e.data.outputSampleRate);
       break;
     case 'disconnect':
       disconnect();
@@ -352,7 +391,3 @@ onmessage = function(e) {
       break;
   }
 };
-
-//free(ptrSamplesOut);
-//free(ptrSampleCount);
-//free(ptrDecoderControl);
